cb4e4356855a695e3413fbee955b04b9c6117176
Pavlos Andreou <pavlosan67@gmail.com>
2019-06-13 14:26:11 +0300

    sixth
    save fixed
    updatestudent fixed

--- main.c
+++ main.c
@@ -26,30 +26,26 @@ int main(int argc, char **argv)
         printMenu();
 
         do {
+            printf("choice: ");
             scanf("%c", &choice);
-        } while( choice < '1' || choice > '8' );
+            printf("\n");
+        } while( choice < '1' || choice > '7' );
 
         switch( choice ) {
             case '1':
-                printf("are you sure you want to load an exising list? [y/n]\n");
-                scanf("%s", &ans);
-                if (ans == 'y')
-                    load(argv[1],studlist);
-
-                break;
-
-            case '2':
                 printf("are you sure you want to save the list? [y/n]\n");
                 scanf("%s", &ans);
                 if (ans == 'y')
                     save(argv[1],studlist); 
                 break;
 
-            case '3':
+            case '2':
                 printstudents(studlist);
                 break;
 
-            case '4':
+            case '3':
+                strcpy(tmp.name, "");
+                tmp.id = 0;
                 printf("give students name:\n");
                 scanf("%s", tmp.name);
                 printf("give student id:\n");
@@ -58,17 +54,17 @@ int main(int argc, char **argv)
                 printf("students added succesfully :)\n");
                 break;
 
-            case '5':
+            case '4':
                 printf("give a student's id\n");
                 scanf("%d", &tmp.id);
                 if (findStudent(tmp.id,studlist) != NULL){
-                    printf("%s", tmp.name);
+                    printf("%s\n", tmp.name);
                 }
                 else 
                     printf("student not fount :(\n");
                 break;
 
-            case '6':
+            case '5':
                 printf("give student id you want to exterminate\n");
                 scanf("%d", &tmp.id);
                 deleteStudent(tmp.id,studlist);
@@ -79,21 +75,34 @@ int main(int argc, char **argv)
                     printf("student succesfully removed from the equation\n");
                 break;
             
-            case '7':
+            case '6':
                 printf("give the id of the student you want to change:\n");
                 scanf("%d", &tmp.id);
                 updateStudent(tmp,studlist); //its either that or tmp.id ( if i do that i will have to change the updatestudent)
                 break;
 
-            case '8':
-                printf("you are about to exit the progrmam make sure you saved any chances...[y/n]\n");
+            case '7':
+                printf("you are about to exit the progrmam. Do you want to save any chances? [y/n]\n");
                 scanf("%s", &ans);
-                if ( ans == 'n')
+                if ( ans == 'n'){
+                    save(argv[1], studlist);
                     break;
+                }
                 else {
                     run=0;
                     break;
                 }
+            
+            default:
+                while (choice < '1' || choice > '7'){
+                    printf("wrong input. try again:\n");
+                    printf("choice: ");
+                    scanf("%c", &choice);
+                    printf("\n");
+                }
+            //end of case
             }
+        //end of switch
         }
+    return 0;
 }
--- student.c
+++ student.c
@@ -6,29 +6,33 @@
 
 void printMenu()
 {
-    printf("Choose from 1 to 7:\n");
+    printf("\x1B[37m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1B[0m");
+    printf("\n\x1B[31mChoose from 1 to 7:\n");
 
-    printf("1. Ανάκτηση λίστας φοιτητών από αρχείο\n");
-    printf("2. Αποθήκευση λίστας φοιτητών σε αρχείο\n");
-    printf("3. Αναπαράσταση λίστας\n");
-    printf("4. Προσθήκη νέου student\n");
-    printf("5. Αναζήτηση του student από το array με βάση το id\n");
-    printf("6. Διαγραφή με βάση το id\n");
-    printf("7. Ανανέωση με βάση το id\n");
-    printf("8. Quit\n");
+    printf("\x1B[36m");
+    printf("1. Αποθήκευση λίστας φοιτητών σε αρχείο\n");
+    printf("2. Αναπαράσταση λίστας\n");
+    printf("3. Προσθήκη νέου student\n");
+    printf("4. Αναζήτηση του student από το array με βάση το id\n");
+    printf("5. Διαγραφή με βάση το id\n");
+    printf("6. Ανανέωση με βάση το id\n");
+    printf("7. Quit\n");
+    printf("\x1B[0m");
+    printf("\x1B[37m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\x1B[0m");
 }
 
 void print(student st)
 {
-    printf("the student is:\n");
-    printf("%d", st.id);
-    printf("%s", st.name);
+    //printf("the student is:\n");
+    printf("%d\t", st.id);
+    printf("%s\n", st.name);
     
 }
 
 void printstudents(list l)
 {
     node currnode = l->head;
+    printf("total students are:\n");
     
     while (currnode != NULL){
         print(currnode->data);
@@ -89,22 +93,26 @@ student* load(char *filename, list l)
 void save(char *filename, list l) 
 {
     FILE* filest = fopen(filename, "w");
+
+    if (filest == NULL){
+        printf("failed to save any changes");
+        exit(1);
+    }
+
     node currnode = l->head;
     student tmp;
     
     while (currnode != NULL){
         tmp.id = currnode->data.id;
         strcpy(tmp.name, currnode->data.name);
-        currnode = currnode->next;
         fwrite(&tmp, sizeof(student), 1, filest);
+        currnode = currnode->next;
     }
     fclose(filest);
 }
 
-
-
 int deleteStudent(int pid, list l){
-    //Doulebei mono gia ton proto foititi
+
     if (findStudent(pid, l) == NULL){
         printf("student not found\n");
         return 0;
@@ -122,8 +130,11 @@ int deleteStudent(int pid, list l){
 
 int updateStudent(student st, list l)
 {
+    node currnode;
+    currnode = findStudent(st.id,l);
+
     int i;
-    if (findStudent(st.id,l)== NULL){
+    if (currnode == NULL){
         printf("the student id cound not be found...");
         return 0;
     }
@@ -131,6 +142,7 @@ int updateStudent(student st, list l)
         printf("give new name for the student %d:", st.id);
         scanf("%s", st.name);
     }
+    strcpy(currnode->data.name, st.name);
 }
 
 list list_create()
@@ -153,18 +165,44 @@ int list_isempty( list l )
 }
 
 void list_push_back( list l, int id, char *name){
-    node currnode = l->head;
-    assert(currnode);
-	strcpy(currnode->data.name, currnode->next->data.name); //help
+    node newNode = (node)malloc(sizeof(struct nodeR));
+	node tmp;
+    strcpy(newNode->data.name,name);
+    newNode->data.id =id;
+    newNode->next = NULL;
 
-    while (currnode != l->tail){
-        if (list_isempty(l)){
-		    l->head = currnode;
-		    l->tail = currnode;
-	    }
-	    else {
-		    l->tail->next = currnode;
-		    l->tail = currnode;
-	    }
+    if (list_isempty(l)){
+		l->head = newNode;
+		l->tail = newNode;
+	}
+	else {
+        l->tail->next = newNode;
+		l->tail = newNode;
+        /*
+        tmp = l->tail;
+        l->tail = currnode;
+		l->tail->next = NULL;
+        tmp->next = currnode;*/
     }
+
+    l->size++;	
 }
+/*
+void list_push_back( list l, char *name )
+{
+	node currnode = malloc(sizeof(struct nodeR));
+	assert(currnode);
+	strcpy(currnode->name, name);
+	currnode->next = NULL;
+
+	if (list_isempty(l)){
+		l->head = currnode;
+		l->tail = currnode;
+	}
+	else {
+		l->tail->next = currnode;
+		l->tail = currnode;
+	}
+
+	l->size++;
+}*/
